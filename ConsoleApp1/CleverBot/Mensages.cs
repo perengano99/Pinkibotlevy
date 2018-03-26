49d509807a86ec808a141d771ff024e84c1ee2b9
perengano99 <35026645+perengano99@users.noreply.github.com>
2018-03-26 07:34:38 -0700

    Add files via upload

---
+++ ConsoleApp1/CleverBot/Mensages.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Levy.CleverBot
+{
+    public class Mensages : ModuleBase<SocketCommandContext>
+    {
+        [Command("levy")]
+        public async Task MensagesAsync()
+        {
+            await ReplyAsync("Que quieres? :v");
+        }
+    }
+}
